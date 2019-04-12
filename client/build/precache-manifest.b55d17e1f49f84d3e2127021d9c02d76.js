55fdfc9a23df8b30b593b1437795693966f92cc9
DevAbas <abas.memmedov.94@gmail.com>
2019-04-12 18:45:49 +0400

    Gitignore file changed

---
+++ client/build/precache-manifest.b55d17e1f49f84d3e2127021d9c02d76.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a22fe0028b810f74cd2d",
+    "url": "/static/css/main.e72ce83d.chunk.css"
+  },
+  {
+    "revision": "a22fe0028b810f74cd2d",
+    "url": "/static/js/main.a22fe002.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "64d30a3abadbb5096053",
+    "url": "/static/css/2.014489a4.chunk.css"
+  },
+  {
+    "revision": "64d30a3abadbb5096053",
+    "url": "/static/js/2.64d30a3a.chunk.js"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "3f3bccc02e3bd73d75f77cab893328e6",
+    "url": "/index.html"
+  }
+];
